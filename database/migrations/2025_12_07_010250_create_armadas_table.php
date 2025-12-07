f21aba0fa7b4a24be83a6b3f676b2ae8efb5c2c4
rahman <safdar23@if.unjani.ac.id>
2025-12-07 08:16:43 +0700

    Membuat migrasi database dan model

---
+++ database/migrations/2025_12_07_010250_create_armadas_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+{
+    Schema::create('armadas', function (Blueprint $table) {
+        $table->id('id_armada');
+        $table->string('nomor_plat')->unique();
+        $table->string('jenis_kendaraan');
+        $table->integer('kapasitas'); // Menggunakan Integer sesuai SDD
+        $table->string('status'); // Tersedia, Terpakai, Perbaikan
+        $table->timestamps();
+    });
+}
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('armadas');
+    }
+};
